2d4c0e01a133d9ae2fa60b2945df9d018e282156
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-12-06 13:21:46 -0500

    basic get playlist thing done

--- README.md
+++ README.md
@@ -5,5 +5,7 @@
 DONT OVER OPTIMIZE. Focus on the business logic/problems. The spotify API isn't even a huge part of the app.
 
 - [ ] Hookup Spotify API service to a route
-- [ ] Make sure spotify API connection gets called on app start.
+- [ ] you dont have to use separate routers/services yet but you should be able to hit the api through the main app.
+- [ ] Add Get playlist endpoint, make sure you can hit it through the main app. Should authenticate and stuff.
+- [ ] Add an integration test/unit test for the get playlist endpoint just so you know the flow.
 - [ ] Add Spotify API
--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -3,16 +3,30 @@ import "dotenv/config";
 
 const clientId = process.env.SPOTIFY_CLIENT_ID as string;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET as string;
+const playlistId: string = "1g2MxzFSWnS2Xz9b8fKAaW?si=2c4f6a8ff8f342c4";
 
 const api = SpotifyApi.withClientCredentials(clientId, clientSecret);
 
 const items = await api.search("The Beatles", ["artist"]);
 
+const playlist = await api.playlists.getPlaylist(
+  playlistId,
+  undefined,
+  "tracks",
+  undefined
+);
 // const playlist = await api.playlists.getPlaylist("")
+// console.table(
+//   items.artists.items.map((item) => ({
+//     name: item.name,
+//     followers: item.followers.total,
+//     popularity: item.popularity,
+//   }))
+// );
+
 console.table(
-  items.artists.items.map((item) => ({
-    name: item.name,
-    followers: item.followers.total,
-    popularity: item.popularity,
+  playlist.tracks.items.map((item) => ({
+    name: item.track.name,
+    artist: item.track.artists.map((artist) => artist.name).join(", "),
   }))
 );
