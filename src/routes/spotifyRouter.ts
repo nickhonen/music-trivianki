86600ad806a5438a2a65a50250c62eaa98100e60
Nicholas Honen <65134752+nickhonen@users.noreply.github.com>
2024-12-10 11:18:17 -0500

    setup testing about to start

--- src/routes/spotifyRouter.ts
+++ src/routes/spotifyRouter.ts
@@ -1,26 +1,26 @@
-import { Router } from "express";
-import * as spotifyService from "../services/spotify.js";
+import { Router } from 'express'
+import * as spotifyService from '../services/spotify.js'
 
-const router = Router();
+const router = Router()
 
-export const TRIVIA_PLAYLIST_ID = "1g2MxzFSWnS2Xz9b8fKAaW";
+const TRIVIA_PLAYLIST_ID = process.env.TRIVIA_PLAYLIST_ID!
 
-router.get("/playlists/:playlistId/tracks", async (req, res) => {
+router.get('/playlists/:playlistId/tracks', async (req, res) => {
   const playlistTracks = await spotifyService.getAllPlaylistTracks(
     req.params.playlistId
-  );
+  )
   if (playlistTracks) {
-    res.json(playlistTracks);
+    res.json(playlistTracks)
   }
-});
+})
 
-router.get("/playlists/:playlistId", async (req, res) => {
+router.get('/playlists/:playlistId', async (req, res) => {
   const playlist = await spotifyService.getPlaylistSongAndArtist(
     req.params.playlistId
-  );
+  )
   if (playlist) {
-    res.json(playlist);
+    res.json(playlist)
   }
-});
+})
 
-export default router;
+export default router
